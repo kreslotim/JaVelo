beef2df7a3a183b860c29b6aa3afc536183d91a7
annhit29 <ann20010929@gmail.com>
2022-02-23 13:38:23 +0100

    ajout WebMercator

---
+++ src/ch/epfl/javelo/projection/WebMercator.java
@@ -0,0 +1,47 @@
+package ch.epfl.javelo.projection;
+
+import ch.epfl.javelo.Math2;
+
+/* classe utilitaire : convertir entre les coordonnées WGS 84 et les coordonnées Web Mercator */
+public final class WebMercator {
+
+    /**
+     * retourne la coordonnée x de la projection d'un point se trouvant à la longitude lon, donnée en radians
+     *
+     * @param lon
+     * @return
+     */
+    public static double x(double lon) {
+        return (lon + Math.PI) / (2 * Math.PI);
+    }
+
+    /**
+     * retourne la coordonnée y de la projection d'un point se trouvant à la latitude lat, donnée en radians
+     *
+     * @param lat
+     * @return
+     */
+    public static double y(double lat) {
+        return (Math.PI - Math2.arsinh(Math.tan(lat))) / (2 * Math.PI); //la fonction arsinh ??
+    }
+
+    /**
+     * retourne la longitude, en radians, d'un point dont la projection se trouve à la coordonnée x donnée
+     *
+     * @param x
+     * @return
+     */
+    public static double lon(double x) {
+        return (2 * Math.PI * x) - Math.PI;
+    }
+
+    /**
+     * retourne la latitude, en radians, d'un point dont la projection se trouve à la coordonnée y donnée
+     *
+     * @param y
+     * @return
+     */
+    public static double lat(double y) {
+        return Math.atan(Math.sinh(Math.PI - 2 * Math.PI * y));
+    }
+}
